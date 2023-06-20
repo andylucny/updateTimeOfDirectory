76d8936743e1e587ee12be9571ce34eb87bc15f2
Andrej Lucny <andylucny@gmail.com>
2023-06-20 23:55:11 +0200

    ver Alpha

---
+++ updateTimeOfDirectory.py
@@ -0,0 +1,48 @@
+import os
+import time
+import PySimpleGUI as sg
+
+def update_directory_creation_dates(directory):
+    dates = []
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if not os.path.isdir(item_path):
+            dates.append(os.path.getmtime(item_path))
+        else:
+            if update_directory_creation_dates(item_path):
+                dates.append(os.path.getmtime(item_path))
+            
+    if len(dates) > 0:
+        newest_file_mtime = max(dates)
+        # Update directory creation date
+        os.utime(directory, (newest_file_mtime, newest_file_mtime))
+        print(f"{directory} -> {time.ctime(newest_file_mtime)}")
+        return True
+    return False
+
+# GUI layout
+layout = [
+    [sg.Text('Select a directory:')],
+    [sg.Input(key='-FOLDER-', enable_events=True), sg.FolderBrowse()],
+    [sg.Button('Update', key='-UPDATE-', disabled=True)],
+    [sg.Output(size=(80, 10))]
+]
+
+window = sg.Window('Update Time Of Directory', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == '-FOLDER-':
+        folder_path = values['-FOLDER-']
+        if folder_path:
+            window['-UPDATE-'].update(disabled=False)
+        else:
+            window['-UPDATE-'].update(disabled=True)
+    elif event == '-UPDATE-':
+        folder_path = values['-FOLDER-']
+        update_directory_creation_dates(folder_path)
+        print("Updated.")
+
+window.close()
